796e36f628d7eefc38d7524fd2e7d722b1e34dbe
Adam Davies <45388618+Alvin76@users.noreply.github.com>
2019-10-28 14:11:36 -0400

    ?

---
+++ StarterGame/StarterGame/Goods/Equipments.cs
@@ -0,0 +1,40 @@
+﻿using DotLiquid.Tags;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StarterGame.Goods
+{
+    enum EquipType{ HEAD, BODY, LEG, SHOES, ARM}
+    class Equipments : Items
+    {
+        // overriding the accessor in super class
+        public override String ItemName { get { return itemName; } set { itemName = value; } } 
+        public override float Volumn { get { return volumn; } set { volumn = value; } }
+        public override float Weight { get { return weight; } set { weight = value; } }
+        public override int Count { get { return count; } set { count = value; } }
+        public override float Worth { get { return worth; } set { worth = value; } }
+
+        // accessor for equipment stat value and type
+        private int attackValue;
+        public int AttackValue { get { return attackValue; } set { attackValue = value; } }
+        private int defenseValue;
+        public int DefenseValue { get { return defenseValue; } set { defenseValue = value; } }
+        private EquipType type;
+        public EquipType Type { get { return type; } set { type = value; } }
+
+        // designated constructor
+        public Equipments(String ItemName, float Volumn, float Weight, int Count, float Worth, int AttackValue, int DefenseValue, EquipType Type)
+        {
+            this.ItemName = ItemName;
+            this.Volumn = Volumn;
+            this.Weight = Weight;
+            this.Count = Count;
+            this.Worth = Worth;
+            this.AttackValue = AttackValue;
+            this.DefenseValue = DefenseValue;
+            this.Type = Type;
+        }
+
+    }
+}
--- StarterGame/StarterGame/Goods/Items.cs
+++ StarterGame/StarterGame/Goods/Items.cs
@@ -7,30 +7,18 @@ namespace StarterGame.Goods
    // enum ItemType { KEYITEM, WEAPON, HEAD, SHIRT, PANTS, SHOES, GLOVES, CONSUMABLE, OTHER}
    // This needs to be abstract or interface this will cause the items that will be objects to be tightly cupoled to this.
    // I like how this is going though.
-    class Items
+   // going to split it into equipment and non-equipment
+    abstract class Items
     {
-        private String itemName;
-        public String ItemName { get { return itemName; } set { itemName = value; } } // itemName accessor
-        private float volumn;
-        public float Volumn { get { return volumn; } set { volumn = value; } } // volumn accessor
-        private float weight;
-        public float Weight { get { return weight; } set { weight = value; } } // weight acessor
-        private int count;
-        public int Count { get { return count; } set { count = value; } } // count accessor
-        private float worth;
-        public float Worth { get { return worth; } set { worth = value; } } // worth acessor
-        private ItemType type;
-        public ItemType Type { get { return type; } set { type = value; } } // type accessor
-
-        // designated constructor
-        public Items(String ItemName, float Volumn, float Weight, int Count, float Worth, ItemType Type)
-        {
-            this.ItemName = ItemName;
-            this.Volumn = Volumn;
-            this.Weight = Weight;
-            this.Count = Count;
-            this.Worth = Worth;
-            this.Type = Type;
-        }
+        protected String itemName;
+        public abstract String ItemName { get; set;} // itemName accessor
+        protected float volumn;
+        public abstract float Volumn { get; set;} // volumn accessor
+        protected float weight;
+        public abstract float Weight { get; set; } // weight acessor
+        protected int count;
+        public abstract int Count { get; set; } // count accessor
+        protected float worth;
+        public abstract float Worth { get; set; } // worth acessor
     }
 }
