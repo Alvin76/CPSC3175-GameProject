9518e8b62b4ba2fcdb10b73e955e8c7ab8e131eb
TLin3795 <t.lin12@yahoo.com>
2019-11-08 00:08:25 -0500

    finishing up nonequipment/interface

--- StarterGame/StarterGame/IGoods/NewNonEquipments.cs
+++ StarterGame/StarterGame/IGoods/NewNonEquipments.cs
@@ -4,23 +4,31 @@ using System.Text;
 
 namespace StarterGame.IGoods
 {
-    enum ifKey { KEY, CONSUMABLE}
+    enum IfKey { KEY, CONSUMABLE}
 
     class NewNonEquipments : IGoods
     {
         // basic accessor fuctions
-        public String ItemName { get; set; }
-        public float Volumn { get; set; }
-        public float Weight { get; set; }
-        public int Count { get; set; }
-        public float Worth { get; set; }
-        public ifKey Key { get; set; }
+        private String itemName;
+        public String ItemName { get { return itemName; } set { itemName = value; } }
+        private float volumn;
+        public float Volumn { get { return volumn; } set { volumn = value; } }
+        private float weight;
+        public float Weight { get { return weight; } set { weight = value; } }
+        private int count;
+        public int Count { get { return count; } set { count = value; } }
+        private float worth;
+        public float Worth { get { return worth; } set { worth = value; } }
+        private IfKey key;
+        public IfKey Key { get { return key; } set { key = value; } }
+        private int hpRecovery;
+        public int HPRecovery { get { return hpRecovery; } set { hpRecovery = value; } }
+        private int mpRecovery;
+        public int MPRecovery { get { return mpRecovery; } set { mpRecovery = value; } }
+        private int tpRecovery;
+        public int TPRecovery { get { return tpRecovery; } set { tpRecovery = value; } }
 
-        public int HPRecovery { get; set; }
-        public int MPRecovery { get; set; }
-        public int TPRecovery { get; set; }
-
-        public NewNonEquipments(String ItemName, float Volumn, float Weight, int Count, float Worth, ifKey Key, int HPRecovery, int MPRecovery, int TPRecovery)
+        public NewNonEquipments(String ItemName, float Volumn, float Weight, int Count, float Worth, IfKey Key, int HPRecovery, int MPRecovery, int TPRecovery)
         {
             this.ItemName = ItemName;
             this.Volumn = Volumn;
