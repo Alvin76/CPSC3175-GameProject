e915333705a7d34e1f55c5b87822765f64f95948
Adam Davies <45388618+Alvin76@users.noreply.github.com>
2019-12-05 18:00:53 -0500

    added trade command

---
+++ StarterGame/StarterGame/Commands/TradeCommand.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StarterGame.Commands
+{
+    //This is used for tradeing 
+    public class TradeCommand : Command
+    {
+        public TradeCommand() : base()
+        {
+            this.name = "Time";
+        }
+        public override bool execute(Characters.Player player)
+        {
+            if (this.hasSecondWord())
+            {
+                player.outputMessage("\nI cannot quit " + this.secondWord);
+
+            }
+            return false;
+        }
+    }
+}
