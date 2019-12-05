7f48f28bbf4c819100089fc4b5354a6890e2b407
Adam Davies <45388618+Alvin76@users.noreply.github.com>
2019-12-05 16:11:23 -0500

    Added back command
    needs to be implemented

---
+++ StarterGame/StarterGame/Commands/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StarterGame.Commands
+{
+    //This is the back command it part of the 5 things needed 
+    public class BackCommand : Command
+    {
+        public BackCommand() : base()
+        {
+            this.name = "attack";
+        }
+        public override bool execute(Characters.Player player)
+        {
+            if (this.hasSecondWord())
+            {
+                player.outputMessage("\nI backup quit " + this.secondWord);
+
+            }
+            return false;
+        }
+    }
+}
